0de13999f69e25e825b60f3c771946f67070aff3
Grzegorz <cymborski@gmail.com>
2021-02-27 11:22:34 +0100

    config done

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import { CBadge } from "@coreui/react";
+import { useState, useEffect } from "react";
 
 const App: React.FC = () => {
-  return <CBadge color="primary"> Primary color badge</CBadge>;
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    setTimeout(() => setCounter(counter + 1), 1000);
+  }, [counter]);
+
+  return (
+    <>
+      <CBadge color="primary"> Primartt34try color badge</CBadge>
+
+      <CBadge color="primary"> {counter}</CBadge>
+    </>
+  );
 };
 
 export default App;
