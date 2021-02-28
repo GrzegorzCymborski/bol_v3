dceace638223871a6f4b012fa5a97e304a8670c2
Grzegorz <cymborski@gmail.com>
2021-02-28 14:45:14 +0100

    added fallback spinner

---
+++ src/components/spinner/Spinner.tsx
@@ -0,0 +1,11 @@
+import { CSpinner } from "@coreui/react";
+
+const Spinner: React.FC = () => {
+  return (
+    <div className="d-flex align-items-center min-vh-100 justify-content-center">
+      <CSpinner color="primary" style={{ width: "40vw", height: "40vw" }} />
+    </div>
+  );
+};
+
+export default Spinner;
