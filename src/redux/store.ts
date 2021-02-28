e36dc69536a4d51b0688e782d048ccd1bedc4de3
Grzegorz <cymborski@gmail.com>
2021-02-28 16:24:16 +0100

    redux serialization check fix

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./user";
 
+import { getDefaultMiddleware } from "@reduxjs/toolkit";
+
 const store = configureStore({
   reducer: {
     user: userReducer,
   },
+  middleware: getDefaultMiddleware({
+    serializableCheck: false,
+  }),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
