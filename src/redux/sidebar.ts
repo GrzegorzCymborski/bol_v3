5ebf7aeec96008d6c595b53e5f72b2edd91c6123
Grzegorz <cymborski@gmail.com>
2021-03-01 19:23:00 +0100

    added sidebar store

---
+++ src/redux/sidebar.ts
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+type SidebarState = {
+  sidebarShow: boolean | "" | "responsive" | undefined;
+};
+
+const initialState: SidebarState = {
+  sidebarShow: "responsive",
+};
+
+export const counterSlice = createSlice({
+  name: "sidebar",
+  initialState,
+  reducers: {
+    changeState: (state, action) => {
+      state.sidebarShow = action.payload;
+    },
+  },
+});
+
+export const { changeState } = counterSlice.actions;
+export default counterSlice.reducer;
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./user";
-
+import sidebarReducer from "./sidebar";
 import { getDefaultMiddleware } from "@reduxjs/toolkit";
 
 const store = configureStore({
   reducer: {
     user: userReducer,
+    sidebar: sidebarReducer,
   },
   middleware: getDefaultMiddleware({
     serializableCheck: false,
