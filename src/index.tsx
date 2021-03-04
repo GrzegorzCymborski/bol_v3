a2f9874867cec12cc9d2c6203377802db19021c1
Grzegorz <cymborski@gmail.com>
2021-03-04 13:49:29 +0100

    added react query

--- src/index.tsx
+++ src/index.tsx
@@ -5,13 +5,19 @@ import { Provider } from "react-redux";
 import store from "./redux/store";
 import { icons } from "./assets/icons";
 import "./styles/styles.scss";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 
 React.icons = icons;
+const queryClient = new QueryClient();
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <App />
+      </QueryClientProvider>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
