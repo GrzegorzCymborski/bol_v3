a4d590a054e8e152cec0f4f7a72ab5223f2649ee
Grzegorz <cymborski@gmail.com>
2021-03-05 20:11:05 +0100

    added products query api

--- src/API.ts
+++ src/API.ts
@@ -1,7 +1,15 @@
 const urlStatsLink = `${process.env.REACT_APP_BASE_URL}/statistics`;
+const apiURL = `${process.env.REACT_APP_BASE_URL}/products?`;
 
 export const statsData: any = async (usrID: string) => {
   const response = await fetch(urlStatsLink, {
+    headers: { Authorization: usrID },
+  });
+  return await response.json();
+};
+
+export const productsData: any = async (usrID: string, queryString: string) => {
+  const response = await fetch(apiURL.concat(queryString), {
     headers: { Authorization: usrID },
   });
   return await response.json();
