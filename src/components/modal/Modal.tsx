68acdc2e243eae775c181203d4434082300bfdd6
Grzegorz <cymborski@gmail.com>
2021-03-08 18:03:36 +0100

    modal component

---
+++ src/components/modal/Modal.tsx
@@ -0,0 +1,18 @@
+import { CModal, CModalHeader, CModalBody } from "@coreui/react";
+
+type ModalProps = {
+  showModal: boolean;
+};
+
+const Modal: React.FC<ModalProps> = ({ showModal }: ModalProps) => {
+  return (
+    <CModal show={showModal} centered>
+      <CModalHeader closeButton>Something went wrong...</CModalHeader>
+      <CModalBody>
+        No records found, please try different search criteria
+      </CModalBody>
+    </CModal>
+  );
+};
+
+export default Modal;
